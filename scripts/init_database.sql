d090100cec379bcd060de5e15db8929c194b54c6
Tausif Kazi <71638420+tkazi2504@users.noreply.github.com>
2025-05-15 12:24:57 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+/*
+This is a starting point where Database and Schema created
+*/
+
+USE master;
+GO
+
+CREATE Datawarehouse;
+
+--CREATE DATAWAREHOUSE
+USE Datawarehouse;
+
+--CREATE SCHEMA Bronze, Silver and Gold
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA  silver;
+GO
+CREATE SCHEMA  gold;
+GO
